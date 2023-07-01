b9110be92ad87d9d0dd1893480b920777e9f2fac
Queenbot524 <135263523+Queenbot524@users.noreply.github.com>
2023-07-01 23:21:18 +0530

    Add files via upload

---
+++ EBDQN/utilities/events/filters.py
@@ -0,0 +1,14 @@
+# Powered By @AdityaHalder
+
+from typing import Union, List
+from pyrogram import filters
+from EBDQN.utilities.config import COMMAND_PREFIXES
+
+
+# ╔══╗╔══╗╔═╦╗╔╦╗╔══╗  ╔═╦═╗╔╦╗╔══╗╔══╗╔═╗
+# ║══╣║╔╗║║║║║║╔╝╚║║╝  ║║║║║║║║║══╣╚║║╝║╔╝
+# ╠══║║╠╣║║║║║║╚╗╔║║╗  ║║║║║║║║╠══║╔║║╗║╚╗
+# ╚══╝╚╝╚╝╚╩═╝╚╩╝╚══╝  ╚╩═╩╝╚═╝╚══╝╚══╝╚═╝
+
+def command(commands: Union[str, List[str]]):
+    return filters.command(commands, COMMAND_PREFIXES)
